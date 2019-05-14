f21e842ad43fb0864c8394751ad3c1a2f610dc7a
romain <romain.thomine007@gmail.com>
2019-05-14 16:00:56 +0200

    vitrine fini

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 // Test
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
