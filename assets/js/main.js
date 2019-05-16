157cf61f1a1a134cb87518869ed472f00cacf57d
sonia <sonia.ferkous@gmail.com>
2019-05-16 11:24:33 +0200

    mediaquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,44 @@
 var eCommApp = angular.module('appComm', ['ngRoute']);
 var total = 0;
+var nbr = 0;
 
 //au demarrage de mon appli
 eCommApp.run(function($rootScope, $http){
   //j'initialise mes tableaux
-    $rootScope.cartList=[];
+  $rootScope.cartList=[];
 
-    $http.get("articles.json")
-    .then(function(response) {
-      // reponse.data correspond au données du JSON et le renvoi dans la variable 'element'
-        $rootScope.element = response.data;
-        console.log($rootScope.element);
-      });
+  $http.get("articles.json")
+  .then(function(response) {
+    // reponse.data correspond au données du JSON et le renvoi dans la variable 'element'
+    $rootScope.element = response.data;
+    console.log($rootScope.element);
+  });
 });
 // Configuration des routes
 eCommApp.config(['$routeProvider', function($routeProvider, $routeParams) {
-			// Système de routage
-			$routeProvider
-			.when('/articles/:cat?', {
-					templateUrl: 'partials/articles.html',
-					controller: 'articleCtrl'
-			})
-			.when('/articles/:art?', {
-					templateUrl: 'partials/articles.html',
-					controller: 'articleCtrl'
-			})
-      .when('/detail/:id?',{
-        controller: 'detailCtrl',
-        templateUrl: 'partials/detail.html'
-      })
-      .when('/panier',{
-        controller: 'panierCtrl',
-        templateUrl: 'partials/panier.html'
-      })
-			.otherwise({
-					redirectTo: '/articles'
-			});
-
-		}
+  // Système de routage
+  $routeProvider
+  .when('/articles/:cat?', {
+    templateUrl: 'partials/articles.html',
+    controller: 'articleCtrl'
+  })
+  .when('/detail/:id?',{
+    controller: 'detailCtrl',
+    templateUrl: 'partials/detail.html'
+  })
+  .when('/panier',{
+    controller: 'panierCtrl',
+    templateUrl: 'partials/panier.html'
+  })
+  .otherwise({
+    redirectTo: '/articles'
+  });
+}
 ]);
 // Création d'un controller 'articleCtrl'
 eCommApp.controller('articleCtrl', function($scope, $rootScope, $routeParams) {
   $scope.filtre=$routeParams.cat;
-  console.log($scope.filtre);
+  // console.log($scope.filtre);
 });
 
 // Création d'un controller 'detailCtrl'
@@ -74,23 +70,57 @@ eCommApp.controller('detailCtrl',function($rootScope,$scope,$routeParams){
       }
     }
     if (test) {
+      let qte = +$('.quantity').val();
+      console.log(qte);
       // console.log($scope.element[$scope.id]);
-      $rootScope.cartList.push($scope.element[$scope.id]);
-      console.log('Panier après ajout :');
-      console.log($rootScope.cartList);
-      console.log('Prix article :');
+      var newArray = $.extend({}, $scope.element[$scope.id], {"Quantite":qte});
+      $rootScope.cartList.push(newArray);
+      // console.log('Panier après ajout :');
+      // console.log($rootScope.cartList);
+      // console.log('Prix article :');
       console.log($scope.element[$scope.id].prix);
-      total += $scope.element[$scope.id].prix;
+      total += $scope.element[$scope.id].prix * qte;
       console.log('Total panier :');
       console.log(total);
-      $routeScope.total = total;
     }
   };
+
 });
 
 // Création d'un controller 'articleCtrl'
-eCommApp.controller('panierCtrl', function($scope) {
+eCommApp.controller('panierCtrl', function($scope, $rootScope) {
 
+  $scope.totalPanier = total;
+
+  $scope.suppCart = function(index){
+    console.log(index);
+    var response = confirm("Voulez-vous vraiment supprimer votre ile ?!");
+    if (response == true) {
+      total -= $rootScope.cartList[index].prix * $rootScope.cartList[index].Quantite;
+      $scope.totalPanier = total;
+      $rootScope.cartList.splice(index, 1);
+    }
+  };
+
+  $scope.addCart = function(index){
+
+  };
+
+  $scope.minusCart = function(index){
+
+  };
+
+  $scope.emptyCart = function(){
+    var response = confirm("Voulez-vous vraiment vider votre panier ?!");
+    if (response == true) {
+      $rootScope.cartList=[];
+      $scope.totalPanier = 0;
+    }
+  };
+
+  $scope.validCart = function(index){
+
+  };
 });
 
 // Fonction pour changer l'image dans les détails
@@ -101,11 +131,14 @@ function changeImage() {
 }
 
 function openNav() {
-    document.getElementById("sideNavigation").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("sideNavigation").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("flc").style.display ="none";
+
 }
 
 function closeNav() {
-    document.getElementById("sideNavigation").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("sideNavigation").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("flc").style.display ="block";
 }
