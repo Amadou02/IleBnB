a35726a192ca05bd4b71cd59f888235a5c38d1fc
Micka <baguet.mickael@gmail.com>
2019-05-16 09:33:26 +0200

    MaJ panier

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var eCommApp = angular.module('appComm', ['ngRoute']);
 var total = 0;
+var nbr = 0;
 
 //au demarrage de mon appli
 eCommApp.run(function($rootScope, $http){
@@ -21,10 +22,6 @@ eCommApp.config(['$routeProvider', function($routeProvider, $routeParams) {
 					templateUrl: 'partials/articles.html',
 					controller: 'articleCtrl'
 			})
-			.when('/articles/:art?', {
-					templateUrl: 'partials/articles.html',
-					controller: 'articleCtrl'
-			})
       .when('/detail/:id?',{
         controller: 'detailCtrl',
         templateUrl: 'partials/detail.html'
@@ -36,13 +33,12 @@ eCommApp.config(['$routeProvider', function($routeProvider, $routeParams) {
 			.otherwise({
 					redirectTo: '/articles'
 			});
-
 		}
 ]);
 // Création d'un controller 'articleCtrl'
 eCommApp.controller('articleCtrl', function($scope, $rootScope, $routeParams) {
   $scope.filtre=$routeParams.cat;
-  console.log($scope.filtre);
+  // console.log($scope.filtre);
 });
 
 // Création d'un controller 'detailCtrl'
@@ -74,23 +70,57 @@ eCommApp.controller('detailCtrl',function($rootScope,$scope,$routeParams){
       }
     }
     if (test) {
+      let qte = +$('.quantity').val();
+      console.log(qte);
       // console.log($scope.element[$scope.id]);
-      $rootScope.cartList.push($scope.element[$scope.id]);
-      console.log('Panier après ajout :');
-      console.log($rootScope.cartList);
-      console.log('Prix article :');
+      var newArray = $.extend({}, $scope.element[$scope.id], {"Quantite":qte});
+      $rootScope.cartList.push(newArray);
+      // console.log('Panier après ajout :');
+      // console.log($rootScope.cartList);
+      // console.log('Prix article :');
       console.log($scope.element[$scope.id].prix);
-      total += $scope.element[$scope.id].prix;
+      total += $scope.element[$scope.id].prix * qte;
       console.log('Total panier :');
       console.log(total);
-      $routeScope.total = total;
     }
   };
+
 });
 
 // Création d'un controller 'articleCtrl'
-eCommApp.controller('panierCtrl', function($scope) {
+eCommApp.controller('panierCtrl', function($scope, $rootScope) {
 
+  $scope.totalPanier = total;
+
+  $scope.suppCart = function(index){
+    console.log(index);
+    var response = confirm("Voulez-vous vraiment supprimer votre ile ?!");
+    if (response == true) {
+      total -= $rootScope.cartList[index].prix * $rootScope.cartList[index].Quantite;
+      $scope.totalPanier = total;
+      $rootScope.cartList.splice(index, 1);
+    }
+  };
+
+  $scope.addCart = function(index){
+
+  };
+
+  $scope.minusCart = function(index){
+
+  };
+
+  $scope.emptyCart = function(){
+    var response = confirm("Voulez-vous vraiment vider votre panier ?!");
+    if (response == true) {
+      $rootScope.cartList=[];
+      $scope.totalPanier = 0;
+    }
+  };
+
+  $scope.validCart = function(index){
+
+  };
 });
 
 // Fonction pour changer l'image dans les détails
